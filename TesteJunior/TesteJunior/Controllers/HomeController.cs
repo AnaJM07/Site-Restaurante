2955f760608b2a5f215500a98a5321357e535a22
AnaJM07 <anamartinis71@gmail.com>
2022-06-06 16:34:46 -0300

    Estrutura HTML

---
+++ TesteJunior/TesteJunior/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TesteJunior.Models;
+
+namespace TesteJunior.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public ActionResult ShowPedidos(string Descricao, string entrada)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TesteJunior/TesteJunior/Models/PedidosModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteJunior.Models
+{
+    public class PedidosModel
+    {
+        public string Descricao { get; set; }
+        public string Entrada { get; set; }
+
+        public string historico { get; set; }
+    }
+}
